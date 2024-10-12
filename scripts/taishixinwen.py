9332e2ee8945825953093b08b50de041cdfb1ee6
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-12 23:58:58 +0800

    Update taishixinwen.py

--- scripts/taishixinwen.py
+++ scripts/taishixinwen.py
@@ -15,13 +15,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/Nasimaktar99/yt-m3u8/main/assets/info.m3u8')
+                print('https://wzi.me/?/mv/playlist.mp4')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/Nasimaktar99/yt-m3u8/main/assets/info.m3u8')
+                print('https://wzi.me/?/mv/playlist.mp4')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
