12858c28276b1643db90166978508db93fcda91b
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-13 09:07:29 +0800

    Update taishixinwen.py

--- scripts/taishixinwen.py
+++ scripts/taishixinwen.py
@@ -1,6 +1,3 @@
-#! /usr/bin/python3
-
-
 import requests
 import os
 import sys
@@ -10,19 +7,16 @@ if 'win' in sys.platform:
     windows = True
 
 def grab(url):
-    response = s.get(url, timeout=15).text
+    response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        response = requests.get(url).text
+        if windows:
+            print('https://wzi.me/?/mv/playlist.mp4')
+            return
+        os.system(f'curl "{url}" > temp.txt')
+        response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            if windows:
-                print('https://wzi.me/?/mv/playlist.mp4')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://wzi.me/?/mv/playlist.mp4')
-                return
+            print('https://wzi.me/?/mv/playlist.mp4')
+            return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
@@ -33,16 +27,14 @@ def grab(url):
             break
         else:
             tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
+    streams = requests.get(link[start:end]).text.split('#EXT')
     hd = streams[-1].strip()
     st = hd.find('http')
     print(hd[st:].strip())
-    #print(f"{link[start : end]}")
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
-s = requests.Session()
 with open('../taishixinwen.txt') as f:
     for line in f:
         line = line.strip()
